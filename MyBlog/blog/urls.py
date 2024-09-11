09e1b8b50e120250e1f386f3ede01adb031008c0
Дмитро <murchyk2007@gmail.com>
2024-09-11 21:11:07 +0300

    lesson11

--- MyBlog/blog/urls.py
+++ MyBlog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import post_list
+from . import views
 
 urlpatterns = [
-    path('', post_list, name='home'),
+    # Список всіх постів
+    path('posts/', views.post_list, name='post_list'),
+    
+    # Детальна сторінка конкретного посту
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    
+    # Пости за автором
+    path('author/<int:author_id>/posts/', views.posts_by_author, name='posts_by_author'),
 ]
--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from .models import Post  # Замініть на відповідну модель, яка містить ваші пости
+from .models import Post, Author  # Замініть на відповідну модель, яка містить ваші пости
 
 def post_list(request):
     posts = Post.objects.all()  # Отримуємо всі пости з бази даних
-    return render(request, 'post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def posts_by_author(request, author_id):
+    author = get_object_or_404(Author, id=author_id)
+    posts = Post.objects.filter(author=author)
+
+    return render(request, 'blog/posts_by_author.html', {'author': author, 'posts': posts})
